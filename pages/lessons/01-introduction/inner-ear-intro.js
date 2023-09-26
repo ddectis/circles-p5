63106d592a3620b43e989f2fa2e9ecab70070a7a
ddectis <dandectis@gmail.com>
2023-09-26 13:45:42 -0700

    time for a break

--- pages/lessons/01-introduction/inner-ear-intro.js
+++ pages/lessons/01-introduction/inner-ear-intro.js
@@ -1,4 +1,3 @@
-import GoBackToContents from '../../../components/go-back-to-contents';
 import Layout from '../../../components/layout';
 import styles from '../../../styles/lesson-template.module.css';
 
@@ -6,11 +5,30 @@ export default function InnerEarIntro(props){
     return (
         <Layout>
             <div className={styles.lesson_content}>
-                <h2 className={styles.lesson_title}>Find your Inner Ear</h2>
-                <p>The process of improvisation can also be called spontaneous composition. 
-                    As I improvise, the engine that provides the melodies I play is what I call 
-                    my <b>Inner Ear</b> 
-                </p>
+                <h1 className={styles.lesson_title}>Find your Inner Ear</h1>
+                <div className={styles.lesson_block}>
+                    <h2>What is your Inner Ear?</h2>
+                    <p>
+                        The process of improvisation can also be called spontaneous composition. Improvisation is the act of writing music on the fly, expressing musical ideas just as quickly as they come to you. But where does that music come from? The way I see it, the engine that provides the notes I play when I'm improvising is what I call my <b>Inner Ear</b>. 
+                        <br/><br/>
+                        When folks say that they "have a song stuck in their head", that song is playing on their Inner Ear. I feel like it comes from the same cognitive faculty that permits us to hear our thoughts in the form of an internal monologue. One of the goals of this course is to hone your internal sound system. The first step towards Playing in the sense we mean in this course is to be able to hear what you want to play. 
+                        <br/> <br />
+                        If you can hear it, you can play it.  
+                    </p>
+                    <h2>How to Access the Inner Ear?</h2>
+                    <p>
+                        Let's start with your favorite part of a favorite song. Something that you know really well. Hold that part in your mind and see if you can imagine what it would sound like to hear it; play it back in your head, so to speak. Use the same parts of your brain that you'd use to have an internal monologue. If you can hear your song playing back in your imagination then congratulations, you've accessed your Inner Ear!
+                        <br/><br/>
+                        
+                    </p>
+                    <h2>What's the point?</h2>
+                    <p>
+                        When I am trying to play interesting improvised parts, I rely heavily on my inner ear.
+                    </p>
+
+
+                </div>
+                
             </div>
         </Layout>
     )
