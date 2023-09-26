8a0e063515bcf9a40fea9a5a877bc200034feff0
ddectis <dandectis@gmail.com>
2023-09-26 09:43:12 -0700

    ugh

---
+++ pages/lessons/01-introduction/glossary.js
@@ -0,0 +1,25 @@
+﻿import Link from 'next/link';
+import Layout from '../../../components/layout';
+import styles from '../../../styles/lesson-template.module.css';
+
+export default function Glossary(props) {
+
+    //voice leading challenge Improve these selections of chord voicing with Voice Leading
+
+    return (
+        <Layout>
+            <div className={styles.lesson_content}>
+                <h2 className={styles.lesson_title}>Glossary of Music Terms</h2>
+                <div className={styles.lesson_block}>
+                    <p>
+                        Yak Yak Yak
+                    </p>
+                </div>
+                
+            </div>
+        </Layout>
+        
+        
+        )
+    
+}
